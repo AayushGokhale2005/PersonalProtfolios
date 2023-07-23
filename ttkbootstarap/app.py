46ae99256c4f4adfdd81645724535b9f51d446f3
Aayush <aayushgokhale2005@outlook.com>
2023-07-23 11:55:18 -0400

    done

--- ttkbootstarap/app.py
+++ ttkbootstarap/app.py
@@ -1,10 +1,17 @@
 from tkinter import *
-from ttkbootstrap.constants import*
 import ttkbootstrap as tb
+import tkinter as tk
+root = tk.Tk()
 
-root = tb.Window(themename="superhero")
+style = tb.Style(theme="darkly")  # Create an instance of Style
+root = style.master  # Get the main application window
 root.title("Test")
 root.geometry("500x500")
 
+# functions
+
+# buttons, labels, etc.
+label = tb.Label(root, text="test", font=("Arial", 24))  # Create a themed label
+label.grid(row=0, column=1, padx=10, pady=10)  # Place the label using grid layout
 
 root.mainloop()
